c127aaba258351051434d8fb1326a40ddb5f81b6
rafaelfccg <rfccg@cin.ufpe.br>
2013-09-22 11:19:33 -0400

    Pacote.java

---
+++ Projeto Comunicacao/src/rede/Pacote.java
@@ -0,0 +1,14 @@
+package rede;
+
+import jogo.Jogo;
+
+public class Pacote {
+	Jogo jogo;
+	boolean status;
+	
+	Pacote (Jogo jogo, boolean status){
+		this.jogo = jogo;
+		this.status = status;
+	}
+	
+}
